0fbd4220c1c80945b19b90fb96351287cbd2836e
Kuba Stokowski <j.stokowskii23@gmail.com>
2026-06-18 19:38:02 +0200

    fixed redirect

--- Web/src/app/core/auth/auth.guard.ts
+++ Web/src/app/core/auth/auth.guard.ts
@@ -1,11 +1,14 @@
 import { inject } from '@angular/core';
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthService);
+  const router = inject(Router);
 
-  return authService.isLoggedIn()
-    ? true
-    : authService.router.navigate(['/login'], { queryParams: { returnUrl: state.url } });
+  if (authService.isLoggedIn()) {
+    return true;
+  }
+
+  return router.createUrlTree(['/auth/login']);
 };
